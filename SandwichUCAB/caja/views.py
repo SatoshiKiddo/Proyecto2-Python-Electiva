9269747be478971d20397d3f3200b1f4d5216d3f
Kiddo <pjfariakiddo@gmail.com>
2021-07-31 10:48:19 -0400

    Inicializacion

--- SandwichUCAB/caja/views.py
+++ SandwichUCAB/caja/views.py
@@ -1,6 +1,16 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Sandwich
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Inicio de caja")
+def index(request, id=0):
+    template = loader.get_template('caja/index.html')
+    if (id):
+        try:
+            sandwiches = Sandwich.objects.get(id_factura=id)
+        except Sandwich.DoesNotExist:
+            raise Http404("No existen Sandwiches con dicha factura")
+    context = { 'Sandwiches' : sandwiches, }
+    return HttpResponse(template.render(context,request))
